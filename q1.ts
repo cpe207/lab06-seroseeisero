fbc245bb584c7315a59086d70b07618cde2c6784
Pongpx <yannafong555@gmail.com>
2024-07-10 18:03:38 +0700

    test

--- q1.ts
+++ q1.ts
@@ -4,11 +4,19 @@ import axios from "axios";
 /* assign interface/type to the function definition properly */
 const getUser = async (userId: number) => {
   /* Your code here */
+  try{
+    const userresp = await axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`);
+    const user = userresp.data;
+    return user.name;
+  }
+  catch(err){
+    return 'INVALID USER ID';
+  }
 };
-
-//test case
-const input1 = 1;
-const input2 = 100;
+  
+  //test case
+  const input1 = 1;
+  const input2 = 100;
 
 //run
 getUser(input1).then((result) => console.log(result));
@@ -16,3 +24,6 @@ getUser(input2).then((result) => console.log(result));
 
 // module.exports = getUser;
 export default getUser;
+
+
+///นายพีรภาส อย่างนะ 660610780
--- q2.ts
+++ q2.ts
@@ -1,9 +1,26 @@
 // const axios = require("axios");
 import axios from "axios";
+import { todo } from "node:test";
 
 /* assign interface/type to the function definition properly */
-const getTodo = async (todoId: number) => {
-  /* Your code here */
+const getTodo = async (todoId: number) => { 
+  
+  try{
+    const usertodo = await axios.get(`https://jsonplaceholder.typicode.com/todos/${todoId}`)
+    const todo = usertodo.data;
+    const ID = todo.userId
+    const userresp = await axios.get(`https://jsonplaceholder.typicode.com/users/${ID}`)
+    const user = userresp.data;
+
+    return {
+      owner: user.name,
+      title: todo.title,
+      completed: todo.completed,
+    }
+  }
+  catch (error) {
+    return 'INVALID TODO ID';
+}
 };
 
 //test case
@@ -17,3 +34,5 @@ getTodo(input2).then((result) => console.log(result));
 getTodo(input3).then((result) => console.log(result));
 
 export default getTodo;
+
+///นายพีรภาส อย่างนะ 660610780
